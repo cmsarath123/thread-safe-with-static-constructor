bf4820c4e71c090e4af412b974b4e5ed4b6a446e
sarathkumarc <cmsarath123@gmail.com>
2020-08-15 20:57:28 +0530

    singleton demo with static constructor no locks

---
+++ singletondemo/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace singletondemo
+{
+    public class Singleton
+    {
+        private static int ObjectsCount = 0;
+        public static Singleton instance = new Singleton();
+        public static Singleton GetInstance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+        static Singleton()
+        {
+
+        }
+        private Singleton()
+        {
+            ObjectsCount++;
+            Console.WriteLine($"instances {ObjectsCount}");
+        }
+
+        public void WriteMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
